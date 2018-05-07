fcf838beed0b8b69b38f6181c3044851c1296590
cristianmafla <cristiancaxi@gmail.com>
2018-05-06 23:22:36 -0500

    cambios de babel es2016 babel-plugin-transform-class-properties

--- src/index.js
+++ src/index.js
@@ -2,9 +2,31 @@ import React, { Component } from "react";
 import { render } from "react-dom"; 
 
     class App extends Component {
+        constructor(props){
+            super(props)
+        this.state = {
+            key:"valor state actual"
+        }
+        }
+
+       
+      /*
+      mifuncion(){
+            this.setState({
+                key:"valor del state despues del click"
+            })
+        }
+      */ 
+        mifuncion = () => {
+            this.setState({
+                key: "valor del state despues del click"
+            })
+        };
+       
         render(){
-            return (
-                <h1>PLANTILLA CODEIGNITER CON REACTJS..</h1>
+            return(
+                //<h1 onClick={this.mifuncion.bind(this)}>{this.state.key}</h1>
+                <h1 onClick={this.mifuncion}>{this.state.key}</h1>
             )
         }
     }
